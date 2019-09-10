4aa9e96f930f592bffd621defedc35961297682a
teej387 <teej387@yahoo.com>
2019-09-09 20:20:58 -0500

    Adds from lecture

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken')
 
 const Users = require('../users/users-model.js');
 
@@ -11,7 +12,11 @@ router.post('/register', (req, res) => {
 
   Users.add(user)
     .then(saved => {
-      res.status(201).json(saved);
+      const token = generateToken(saved)
+      res.status(201).json({
+        user: saved,
+        token
+      });
     })
     .catch(error => {
       res.status(500).json(error);
@@ -25,8 +30,12 @@ router.post('/login', (req, res) => {
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
+        // Generate a jwt
+        const token = generateToken(user)
+        
         res.status(200).json({
           message: `Welcome ${user.username}!`,
+          token
         });
       } else {
         res.status(401).json({ message: 'Invalid Credentials' });
@@ -36,5 +45,18 @@ router.post('/login', (req, res) => {
       res.status(500).json(error);
     });
 });
+
+function generateToken(user) {
+  const payload = {
+    sub: user.id,
+    username: user.username
+  }
+
+  const options = {
+    expiresIn: '1d'
+  }
+
+  return jwt.sign(payload, process.env.JWT_SECRET, options)
+}
 
 module.exports = router;
